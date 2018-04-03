2150dd5eba70833e1206d12ac68b5e1f1ec38887
Kyunianti19 <37916598+Kyunianti19@users.noreply.github.com>
2018-04-03 21:57:45 +0700

    Update Function.cpp
    Akhirnya kujadikan 3 fungsi berbeda ._. On, Off, sama Delay

--- Function.cpp
+++ Function.cpp
@@ -6,15 +6,40 @@
 #include "Arduino.h"
 #include "Function.h"
 
-void Function::OnOffDelay (string pilihan)
+//Fungsi buat menghidupkan lampu built in
+void Function::on ()
+{
+  digitalWrite (13, HIGH);
+}
+
+//Fungsi buat mematikan lampu built in
+void Function::off ()
+{
+  digitalWrite (13, LOW);
+}
+
+//Fungsi buat bikin lampu kedap-kedip dengan delay
+void Function::delay (string pilihan)
 {
   _pilihan = pilihan;
-  if (_pilihan == "on") {
-    digitalWrite (13, HIGH);
-    
+  _idx = _pilihan.indexOf(" ");     //Mencari lokasi "spasi" pada string "delay x" atau "d x"
+  _pilihan = _pilihan.substring(_idx+1);      //Memotong string pilihan, di ambil bagian yang ada angkanya (hanya mengambil x)
+  _waktuDelay = _pilihan.toInt();     //Mengubah string menjadi integer
+  
+  //Kedipan hanya terlihat ketika lampu menyala dan delay tidak sama dengan nol
+  //asumsi input User pada delay selalu >=0 (tidak bisa negatif)
+  if (digitalRead(13) == HIGH) {
+    if (_waktuDelay == 0) {
+      digitalWrite (13, HIGH);
+    }
+    else {
+      digitalWrite (13, LOW);
+      delay (_waktuDelay);
+      digitalWrite (13, HIGH);
+      delay (_waktuDelay);
+    }
   }
-  else if (_pilihan == "off") {
+  else if (digitalRead(13) == LOW) {
     digitalWrite (13, LOW);
-    
   }
 }
